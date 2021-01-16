acc14e68daf2c6c658efd3a0fd86b2c809f0b6cf
gwn <dev@gwn.wtf>
2021-01-16 18:03:49 +0300

    Remove unused crypto utils

--- test/util/cryptoUtil.ts
+++ test/util/cryptoUtil.ts
@@ -1,54 +1,8 @@
 import {ISovrinDidModel} from '../../src/common/models';
 
-const bip39 = require('bip39');
 const sovrin = require('sovrin-did');
-const SHA256 = require('crypto-js/sha256');
-const base58 = require('bs58');
-const EC = require('elliptic').ec;
-const hash = require('json-hash');
 
 class CryptoUtil {
-  generateMnemonic() {
-    return bip39.generateMnemonic();
-  }
-
-  generateSovrinDID(mnemonic: string) {
-    const seed = SHA256(mnemonic).toString();
-
-    // Convert SHA256 hash to Uint8Array
-    var didSeed = new Uint8Array(32);
-    for (var i = 0; i < 32; ++i) {
-      didSeed[i] = parseInt(seed.substring(i * 2, i * 2 + 2), 16);
-    }
-
-    // Create the Sovrin DID
-    return sovrin.fromSeed(didSeed);
-  }
-
-  getDocumentSignature(privateKey: string, publicKey: string, inputFile: string) {
-    return base58.encode(sovrin.signMessage(new Buffer(JSON.stringify(inputFile)), privateKey, publicKey));
-  }
-
-  verifyDocumentSignature(signature: string, publicKey: string) {
-    return !(sovrin.verifySignedMessage(base58.decode(signature), publicKey) === false);
-  }
-
-  generateEcdsaKeyPair() {
-    var ec = new EC('secp256k1');
-    return ec.genKeyPair();
-  }
-
-  signPayloadUsingEcdsaKey(payload: any, ecdsaKey: any) {
-    var payloadHash = hash.digest(payload);
-    return ecdsaKey.sign(payloadHash);
-  }
-
-  verifyEcdsaSignature(key: any, signature: any, payload: any): boolean {
-    var derSignature = signature.toDER();
-    var payloadHash = hash.digest(payload);
-    return key.verify(payloadHash, derSignature);
-  }
-
   getSignatureFromSignatureValue(sovrinDid: ISovrinDidModel, signatureValue: any) {
     return {
       type: 'ed25519-sha-256',
